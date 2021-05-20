ba5870ad93c24dcdccd03c577b661013a60e0409
Robyn Taylor <rhtaylor0715@gmail.com>
2021-05-20 16:27:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,91 +1,57 @@
-require 'pry'
-
 class Song
   attr_accessor :name, :artist_name
-      #provides a .name= method and a .name method
   @@all = []
-      #@@all will store every instance of the Song class
-
+ 
   def self.all
-      #self.all is a class method for reading the data stored in a
-      #class variable (i.e. this method will read @@all)
     @@all
   end
-
+ 
   def save
     self.class.all << self
   end
-
-
+ 
   def self.create
-      #class constructor is a unique method which is invoked when
-      #an object of that particular class is being instantiated
-    song = self.new
-      #creating an new instance of the Song class
+    song = Song.new
     @@all << song
-      #saves the new instance to @@all, which can be called literally
-      #or using the .all method (self.all)
     song
-      #returns the song instance that was initialized and saved
   end
-
-
-  def self.new_by_name(song_title)
+  
+  def self.new_by_name(song_name)
     song = self.new
-    song.name = song_title
+      #instantiating a new song
+    song.name = song_name
+      #instantiating the new song with name propery
     song
   end
-
-
-  def self.create_by_name(song_title)
+ 
+  def self.create_by_name(song_name)
     song = self.create
-    song.name = song_title
+    song.name = song_name
     song
   end
   
-  
-  def self.find_by_name(song_title)
-    self.all.find { |song| song.name == song_title }
+  def self.find_by_name(song_name)
+    self.all.find { |song| song.name == song_name }
   end
-  
-  
-  def self.find_or_create_by_name(song_title)
-    search_result = self.find_by_name(song_title)
-    if search_result
-      search_result
+
+  def self.find_or_create_by_name(song_name)
+    result = self.find_by_name(song_name)
+    if result
+      result
     else
-      self.create_by_name(song_title)
+      self.create_by_name(song_name)
     end
   end
-  
-  
+ 
   def self.alphabetical
-    abc_sorted = self.all.sort_by { |song| song.name }
-    abc_sorted
+    self.all.sort_by { |song_name| 
   end
+    
+    
+    
 
-
-  def self.new_from_filename(file_name)
-    song_array = file_name.split(" - ")
-    song_array[1] = song_array[1].chomp(".mp3")
-    song = self.new
-    song.name = song_array[1]
-    song.artist_name = song_array[0]
-    song
-  end
-
-
-  def self.create_from_filename(file_name)
-    song_array = file_name.split(" - ")
-    song_array[1] = song_array[1].chomp(".mp3")
-    song = self.create
-    song.name = song_array[1]
-    song.artist_name = song_array[0]
-    song
-  end
-  
-  
-  def self.destroy_all
-    self.all.clear
-  end
+ 
+ 
+ 
+ 
 end
